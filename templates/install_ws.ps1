726092934d393a4594e942e5bfbd072e2f19ce05
devoptimist <sbrown@chef.io>
2019-05-21 19:14:36 +0100

    fixing version typo

--- templates/install_ws.ps1
+++ templates/install_ws.ps1
@@ -6,5 +6,5 @@ choco install habitat --version ${hab_version} -y
 choco install git -y
 choco install googlechrome -y
 choco install vscode -y
-choco install chef-workstation --versio ${chef_workstation_version} -y
+choco install chef-workstation --version ${chef_workstation_version} -y
 hab_accept
